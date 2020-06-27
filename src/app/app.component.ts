f798e1228cf3c2ef1628723650cbbec994a08442
Andrei Voicu <dreivoicu@gmail.com>
2020-06-27 16:06:36 +0300

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  form: FormGroup;
-  private unsubscribe = new Subject<void>();
-
-  constructor() {
-    this.form = new FormGroup({});
-  }
-
-  ngOnInit(): void {
-    this.form.valueChanges
-      .pipe(takeUntil(this.unsubscribe))
-      .subscribe(console.log);
-  }
-
-  ngOnDestroy(): void {
-    this.unsubscribe.next();
-  }
-
-  onSubmit(): void {
-    alert('submitted');
-  }
+export class AppComponent {
 }
