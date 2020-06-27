5235d8ac47a95acb3dd4b953149be4b3664526d8
Andrei Voicu <dreivoicu@gmail.com>
2020-06-27 15:57:32 +0300

    rename

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-upload-to-firebase'),
+      dir: require('path').join(__dirname, './coverage/ng-gram'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
