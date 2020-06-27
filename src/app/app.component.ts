da5799067aac164b7e1aedc295ab888dd6b4ba91
Andrei Voicu <dreivoicu@gmail.com>
2020-06-27 13:42:10 +0300

    lint + formatting + CI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-upload-to-firebase';
-}
+export class AppComponent {}
