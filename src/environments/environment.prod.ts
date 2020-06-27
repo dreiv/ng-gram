045bfb368eac1e13295455b78da5f631653ad8b9
Andrei Voicu <dreivoicu@gmail.com>
2020-06-27 19:07:53 +0300

    firebase configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCLDwy6injE-BgpkePc39znlbtqk5u0iFE'
+    apiKey: 'AIzaSyCLDwy6injE-BgpkePc39znlbtqk5u0iFE',
+    authDomain: 'nggram-f227d.firebaseapp.com',
+    databaseURL: 'https://nggram-f227d.firebaseio.com',
+    projectId: 'nggram-f227d',
+    storageBucket: 'nggram-f227d.appspot.com',
+    messagingSenderId: '65067620859',
+    appId: '1:65067620859:web:b427e685bf5c7d7fc3d35e'
   }
 };
